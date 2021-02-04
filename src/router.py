f896b2ea513e011d39d468efaf2f9bf3ee669153
Witsarut <61070210@it.kmitl.ac.th>
2021-02-04 16:05:02 +0700

    net commit

--- src/router.py
+++ src/router.py
@@ -6,13 +6,28 @@ class Router:
         self.interface = {}
 
     def add_int(self, int_name):
-        self.interface[int_name] = ['-', '-']
+        self.interface[int_name] = ['-', 'not connect to any neighbor']
 
     def add_ip(self, int_name, ip):
         self.interface[int_name][0] = ip
 
-    def add_connect(self, int_name, connect):
-        self.interface[int_name][1] = connect
+    def add_connect(self, from_int, to_int, device):
+        self.interface[from_int][1] = "connect to device " + device.hostname + " interface " + to_int 
+        device.interface[to_int][1] = "connect to device " + self.hostname + " interface " + from_int 
+
+    def change_hostname(self, new_name):
+        self.hostname = new_name
+
+    def show_int(self):
+        print('Show interfaces of ' + self.hostname)
+        print(self.hostname + " has " + str(len(self.interface)) + " interfaces")
+        for att in self.interface:
+            print(att)
+        
+    def show_neighbor(self):
+        for att in self.interface:
+            print(att + " " + self.interface[att][1])
+        print('\n')
 
 
        
@@ -34,15 +49,28 @@ r2.add_ip('Serial 0/2', '192.168.4.1')
 r2.add_ip('Serial 0/3', '192.168.5.1')
 r3.add_ip('Serial 0/1', '192.168.6.1')
 
-r1.add_connect('Serial 0/1', r2.interface['Serial 0/2'][1])
-r1.add_connect('Serial 0/2', r3.interface['Serial 0/1'][1])
+r1.add_connect('Serial 0/1', 'Serial 0/2', r2)
+r1.add_connect('Serial 0/2', 'Serial 0/1', r3)
 
+r1.change_hostname("R1-NEW")
+r2.change_hostname("R2-NEW")
+r3.change_hostname("R3-NEW")
+
+
+
+r1.show_int()
+r1.show_neighbor()
+
+r2.show_int()
+r2.show_neighbor()
+
+
+r3.show_int()
+r3.show_neighbor()
 
 # count interfaces
 # print('Show interfaces of '+ r1.hostname + '\n' + r1.hostname + ' has ' + r1.interface + 'interfaces \n')
-print(r1.interface)
-print(r2.interface['Serial 0/1'][0])
-print(r3.interface['Serial 0/1'][0])
+
 
 
 
